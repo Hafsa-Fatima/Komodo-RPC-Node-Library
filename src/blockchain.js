ac594fbf915fc4b5c5b2ec6fa737077904be7a97
hafsa <hafsa.fatima995@gmail.com>
2019-07-31 15:34:36 -0500

    blockchain tested

--- src/blockchain.js
+++ src/blockchain.js
@@ -306,20 +306,34 @@ Connect.prototype.getchaintips = function(){
   return rpc.rpc_request(dataString, this.json)
 }
 
-Connect.prototype.getchaintxstats = function(){
+Connect.prototype.getchaintxstats = function(nblocks,blockhash){
   /*
+  The method getchaintxstats returns statistics about
+  the total number and rate of transactions in the chain.
 
   Arguments:
+  nblocks 	(numeric, optional) 	the number of blocks in the averaging window.
+  blockhash 	(string, optional) 	the hash of the block which ends the window
 
   Response:
+  "time" 	(numeric) 	the timestamp for the final block in the window in UNIX format
+  "txcount" 	(numeric) 	the total number of transactions in the chain up to this point
+  "window_final_block_hash" 	(string) 	the hash of the final block in the window
+  "window_block_count" 	(numeric) 	the size of the window in the number of blocks
+  "window_tx_count" 	(numeric) 	the number of transactions in the window; this value is only returned if window_block_count is > 0.
+  "window_interval" 	(numeric) 	the elapsed time in the window in seconds; this value is only returned if window_block_count is > 0.
+  "txrate" 	(numeric) 	the average rate of transactions per second in the window; this value is only returned if window_interval is > 0.
 
   Examples:
+  conn.getchaintxstats()
 
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
   dataString.method = "getchaintxstats";
-  if(dataString)
-    dataString.params = []
+  if(nblocks && blockhash)
+    dataString.params = [nblocks,blockhash]
+  else if(nblocks)
+    dataString.params = [nblocks]
   else
     dataString.params = []
   return rpc.rpc_request(dataString, this.json)
@@ -327,37 +341,55 @@ Connect.prototype.getchaintxstats = function(){
 
 Connect.prototype.getdifficulty = function(){
   /*
+  The getdifficulty method returns the proof-of-work difficulty
+  as a multiple of the minimum difficulty.
 
   Arguments:
+  (none)
 
   Response:
+  number 	(numeric) 	the proof-of-work difficulty as a multiple
+  of the minimum difficulty
 
   Examples:
+  conn.getdifficulty()
 
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
   dataString.method = "getdifficulty";
-  if(dataString)
-    dataString.params = []
-  else
-    dataString.params = []
+  dataString.params = []
   return rpc.rpc_request(dataString, this.json)
 }
 
-Connect.prototype.getlastsegidstakes = function(){
+Connect.prototype.getlastsegidstakes = function(depth){
   /*
+  The getlastsegidstakes method returns an object
+  containing the number of blocks staked by each
+  segid in the last X number of blocks, where
+  the value of X is equal to the indicated depth.
+  Only applies to -ac_staked Smart Chains
 
-  Arguments:
+  Arguments
+  Name 	Type 	Description
+  depth 	(numeric, required) 	the number of blocks
+          to scan, starting from the current height
+          and working backwards
 
   Response:
+  Name 	Type 	Description
+  "NotSet" 	(numeric) 	the number of blocks that have no SegId set
+  "PoW" 	(numeric) 	the number of blocks created through PoW
+  "PoSPerc" 	(numeric) 	the percentage of blocks created through PoS
+  "SegIds" 	(json object) 	the json containing the data of number of blocks in each SegId
+  "n" 	(numeric) 	the number of blocks staked from SegId n in the last X blocks, where X is equal to the indicated depth
 
   Examples:
-
+  conn.getlastsegidstakes(10)
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
   dataString.method = "getlastsegidstakes";
-  if(dataString)
-    dataString.params = []
+  if(depth)
+    dataString.params = [depth]
   else
     dataString.params = []
   return rpc.rpc_request(dataString, this.json)
@@ -365,12 +397,19 @@ Connect.prototype.getlastsegidstakes = function(){
 
 Connect.prototype.getmempoolinfo = function(){
   /*
+  The getmempoolinfo method returns details on the active
+  state of the transaction memory pool.
 
   Arguments:
+  (none)
 
   Response:
+  "size" 	(numeric) 	the current transaction count
+  "bytes" 	(numeric) 	the sum of all transaction sizes
+  "usage" 	(numeric) 	the total memory usage for the mempool
 
   Examples:
+  conn.getmempoolinfo()
 
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
@@ -382,77 +421,139 @@ Connect.prototype.getmempoolinfo = function(){
   return rpc.rpc_request(dataString, this.json)
 }
 
-Connect.prototype.getrawmempool = function(){
+Connect.prototype.getrawmempool = function(verbose){
   /*
+  The getrawmempool method returns all transaction ids in
+  the memory pool as a json array of transaction ids.
+
+  The verbose input is optional and is false by default.
+  When it is true, the method instead returns a json object
+  with various related data.
 
   Arguments:
+  verbose 	(boolean, optional, default=false) 	true for a json object, false for a json array of transaction ids
 
-  Response:
+  Response: (verbose = false)
+  "transaction_id" 	(string) 	the transaction id
+
+  Response: (verbose = true)
+  "transaction_id": { .... } 	(json object)
+  "size" 	(numeric) 	the transaction size in bytes
+  "fee" 	(numeric) 	the transaction fee
+  "time" 	(numeric) 	the local time transaction entered pool in seconds since 1 Jan 1970 GMT
+  "height" 	(numeric) 	the block height wherein the transaction entered the mempool
+  "startingpriority" 	(numeric) 	the priority when the transaction entered the mempool
+  "currentpriority" 	(numeric) 	the transaction priority at the current height
+  "depends": { ... } 	(array) 	unconfirmed transactions used as inputs for this transaction
+  "transaction_id" 	(string) 	the parent transaction id
 
   Examples:
-
+  conn.getrawmempool(true)
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
   dataString.method = "getrawmempool";
-  if(dataString)
-    dataString.params = []
+  if(verbose)
+    dataString.params = [verbose]
   else
     dataString.params = []
   return rpc.rpc_request(dataString, this.json)
 }
 
-Connect.prototype.getspentinfo = function(){
+Connect.prototype.getspentinfo = function(txid,index=0){
   /*
+  The getspentinfo method returns the transaction id
+  and index where the given output is spent.
+  The method requires spentindex to be enabled.
 
   Arguments:
+  Name 	Type 	Description
+  "txid" 	(string) 	the hex string of the transaction id
+  "index" 	(number) 	the output's index
 
   Response:
+  Name 	Type 	Description
+  "txid" 	(string) 	the transaction id
+  "index" 	(number) 	the spending input index
 
   Examples:
-
+  conn.getspentinfo("51233ca562180a0c32e1977624320526b7d4026b40fc46832473e08452265fad",1)
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
   dataString.method = "getspentinfo";
-  if(dataString)
-    dataString.params = []
+  if(txid)
+    dataString.params = [{"txid":txid,"index":index}]
   else
     dataString.params = []
   return rpc.rpc_request(dataString, this.json)
 }
 
-Connect.prototype.gettxout = function(){
+Connect.prototype.gettxout = function(txid,vout,includemempool=false){
   /*
+  The gettxout method returns details about an unspent transaction output.
 
   Arguments:
+  "txid" 	(string, required) 	the transaction id
+  vout 	(numeric, required) 	the vout value
+  includemempool 	(boolean, optional) 	whether to include the mempool
 
   Response:
+  "bestblock" 	(string) 	the block hash
+  "confirmations" 	(numeric) 	a confirmation number that is aware of the dPoW security service aware
+  "rawconfirmations" 	(numeric) 	the raw confirmations (number of blocks on top of this block with this transaction)
+  "value" 	(numeric) 	the transaction value
+  "scriptPubKey": 	(json object)
+  "asm" 	(string) 	scriptPubKey in assembly format
+  "hex" 	(string) 	scriptPubKey in hex format
+  "reqSigs" 	(numeric) 	the number of required signatures
+  "type" 	(string) 	the type, e.g. pubkeyhash
+  "addresses" 	(array of strings) 	an array of Komodo addresses
+  "address" 	(string) 	the blockchain address
+  "version" 	(numeric) 	the version
+  "coinbase" 	(boolean) 	whether this is a coinbase transaction
 
   Examples:
+  conn.gettxout("51233ca562180a0c32e1977624320526b7d4026b40fc46832473e08452265fad",0,false)
+  conn.gettxout("51233ca562180a0c32e1977624320526b7d4026b40fc46832473e08452265fad",0,true)
+  conn.gettxout("51233ca562180a0c32e1977624320526b7d4026b40fc46832473e08452265fad",1)
 
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
   dataString.method = "gettxout";
-  if(dataString)
-    dataString.params = []
+  if(txid && vout || vout == 0)
+    dataString.params = [txid,vout,includemempool]
   else
     dataString.params = []
   return rpc.rpc_request(dataString, this.json)
 }
 
-Connect.prototype.gettxoutproof = function(){
+Connect.prototype.gettxoutproof = function(txid,blockhash){
   /*
+  The gettxoutproof method returns a hex-encoded proof
+  showing that the indicated transaction was included in a block.
+  The gettxoutproof method relies on the txindex runtime
+  parameter. This parameter is enabled by default on all
+  KMD-based blockchains, and should never be disabled.
 
-  Arguments:
-
-  Response:
+  Arguments
+  "txid" 	(string) 	a transaction hash
+  "blockhash" 	(string, optional) 	if specified, the method
+                looks for the relevant transaction id in this
+                block hash
+  Response
+  "data" 	(string) 	a string that is a serialized,
+          hex-encoded data for the proof
 
   Examples:
+  conn.gettxoutproof(["51233ca562180a0c32e1977624320526b7d4026b40fc46832473e08452265fad"])
+  conn.gettxoutproof(["51233ca562180a0c32e1977624320526b7d4026b40fc46832473e08452265fad"],"0a28e2fb630b282138bf23bb79f597b11acff6f57c8d9c1c10fa54770035c813")
 
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
   dataString.method = "gettxoutproof";
-  if(dataString)
-    dataString.params = []
+  if(txid && blockhash)
+    dataString.params = [txid,blockhash]
+  else if(txid)
+    dataString.params = [txid]
   else
     dataString.params = []
   return rpc.rpc_request(dataString, this.json)
@@ -460,132 +561,223 @@ Connect.prototype.gettxoutproof = function(){
 
 Connect.prototype.gettxoutsetinfo = function(){
   /*
+  The gettxoutsetinfo method returns statistics
+   about the unspent transaction output set.
+  Note this call may take a long time to complete,
+   depending on the state of your blockchain.
 
-  Arguments:
+  Arguments
+  (none)
 
-  Response:
+  Response
+  "height" 	(numeric) 	the current block height (index)
+  "bestblock" 	(string) 	the best block hash hex
+  "transactions" 	(numeric) 	the number of transactions
+  "txouts" 	(numeric) 	the number of output transactions
+  "bytes_serialized" 	(numeric) 	the serialized size
+  "hash_serialized" 	(string) 	the serialized hash
+  "total_amount" 	(numeric) 	the total amount
+  #
+  Examples:
+  conn.gettxoutsetinfo()
+  */
+  let dataString = {"jsonrpc": "1.0", "id":"curltest"};
+  dataString.method = "gettxoutsetinfo";
+  dataString.params = []
+  return rpc.rpc_request(dataString, this.json)
+}
+
+Connect.prototype.kvsearch = function(key){
+  /*
+  The kvsearch method searches for
+  a key stored via the kvupdate command.
+  This feature is only available for Smart Chains.
+
+  Arguments
+  key 	(string, required) 	the key for which the user desires to search the chain
+
+  Response
+  "coin" 	(string) 	the chain on which the key is stored
+  "currentheight" 	(numeric) 	the current height of the chain
+  "key" 	(string) 	the key
+  "keylen" 	(string) 	the length of the key
+  "owner" 	(string) 	a hex string representing the owner of the key
+  "height" 	(numeric) 	the height at which the key was stored
+  "expiration" 	(numeric) 	the height at which the key will expire
+  "flags" 	(numeric) 	1 if the key was created with a password; 0 otherwise
+  "value" 	(string) 	the stored value
+  "valuesize" 	(string) 	the amount of characters stored
 
   Examples:
 
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
-  dataString.method = "";
-  if(dataString)
-    dataString.params = []
+  dataString.method = "kvsearch";
+  if(key)
+    dataString.params = [key]
   else
     dataString.params = []
   return rpc.rpc_request(dataString, this.json)
 }
 
-Connect.prototype.kvsearch = function(){
+Connect.prototype.kvupdate = function(key,value,days,passphrase=''){
   /*
+  The kvupdate method stores a key/value pair via OP_RETURN.
+  This feature is available only for Smart Chains.
+  The maximum value memory size is 8kB.
 
-  Arguments:
+  Arguments
+  Name 	Type 	Description
+  "key" 	(string, required) 	key (should be unique)
+  "value" 	(string, required) 	value
+  "days" 	(numeric, required) 	amount of days before the key expires (1440 blocks/day); minimum 1 day
+  "passphrase" 	(string, optional) 	passphrase required to update this key
 
-  Response:
+  Response
+  Name 	Type 	Description
+  "coin" 	(string) 	the chain on which the key is stored
+  "height" 	(numeric) 	the height at which the key was stored
+  "expiration" 	(numeric) 	the height at which the key will expire
+  "flags" 	(string) 	the amount of days the key will be stored
+  "key" 	(numeric) 	the stored key
+  "keylen" 	(numeric) 	the length of the key
+  "value" 	(numeric) 	the stored value
+  "valuesize" 	(string) 	the length of the stored value
+  "fee" 	(string) 	the transaction fee paid to store the key
+  "txid" 	(string) 	the transaction id
 
   Examples:
-
+  conn.kvupdate("create","10",1000)
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
-  dataString.method = "";
-  if(dataString)
-    dataString.params = []
+  dataString.method = "kvupdate";
+  if(key && value && days)
+    dataString.params = [key,value,days.toString(),passphrase]
   else
     dataString.params = []
   return rpc.rpc_request(dataString, this.json)
 }
 
-Connect.prototype.kvupdate = function(){
+Connect.prototype.minerids = function(height){
   /*
-
-  Arguments:
-
-  Response:
-
+  The minerids method returns information about the
+  notary nodes and external miners at a specific block
+  height. The response will calculate results according
+  to the 2000 blocks proceeding the indicated "height" block.
+  #
+  Arguments
+  Name 	Type 	Description
+  heights 	(number) 	the block height for the query
+  #
+  Response
+  Name 	Type 	Description
+  "mined":
+  "notaryid" 	(number) 	the id of the specific notary node
+  "kmdaddress" 	(string) 	the KMD address of the notary node
+  "pubkey" 	(string) 	the public signing key of the notary node
+  "blocks" 	(number)
+  #
   Examples:
+  conn.minerids(1000)
 
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
-  dataString.method = "";
-  if(dataString)
-    dataString.params = []
+  dataString.method = "minerids";
+  if(height)
+    dataString.params = [height.toString()]
   else
     dataString.params = []
   return rpc.rpc_request(dataString, this.json)
 }
 
-Connect.prototype.minerids = function(){
+Connect.prototype.notaries = function(height,timestamp){
   /*
+  The notaries method returns the public key, BTC address,
+   and KMD address for each Komodo notary node.
+  Either or both of the height and timestamp parameters will suffice.
 
-  Arguments:
+  Arguments
+  Name 	Type 	Description
+  height 	(number) 	the block height desired for the query
+  timestamp 	(number) 	the timestamp of the block desired for the query
 
-  Response:
+  Response
+  Name 	Type 	Description
+  "notaries": [ ... ] 	(array)
+  "pubkey" 	(string) 	the public signing key of the indicated notary node, used on the KMD network to create notary-node authorized transactions
+  "BTCaddress" 	(string) 	the public BTC address the notary node uses on the BTC blockchain to create notarizations
+  "KMDaddress" 	(string) 	the public KMD address the notary node uses on the KMD blockchain to create notarizations
+  "numnotaries" 	(number) 	the number of notary nodes; typically this value is 64, but the value may vary on rare circumstances, such as during election seasons
+  "height" 	(number) 	the block height number at which the notary-node information applies
+  "timestamp" 	(number) 	the timestamp at which the notary-node information applies
 
   Examples:
-
+  conn.notaries(1000)
+  conn.notaries(100,1536365515)
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
-  dataString.method = "";
-  if(dataString)
-    dataString.params = []
+  dataString.method = "notaries";
+  if(height && timestamp)
+    dataString.params = [height.toString(),timestamp.toString()]
+  else if(height)
+    dataString.params = [height.toString()]
+  else if(timestamp)
+    dataString.params = [timestamp.toString()]
   else
     dataString.params = []
   return rpc.rpc_request(dataString, this.json)
 }
 
-Connect.prototype.notaries = function(){
+Connect.prototype.verifychain = function(checklevel,numblocks){
   /*
+  The verifychain method verifies the coin daemon's blockchain database.
 
-  Arguments:
-
-  Response:
+  Depending on the state of your blockchain database and daemon, this call can take a prolonged period of time to complete.
+  #
+  Arguments
+  Name 	Type 	Description
+  checklevel 	(numeric, optional, 0-4, default=3) 	indicates the thoroughness of block verification
+  numblocks 	(numeric, optional, default=288, 0=all) 	indicates the number of blocks to verify
+  #
+  Response
+  Name 	Type 	Description
+  true/false 	(boolean) 	whether the verification was successful
 
   Examples:
-
+  conn.verifychain(1,3)
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
-  dataString.method = "";
-  if(dataString)
-    dataString.params = []
+  dataString.method = "verifychain";
+  if(checklevel && numblocks || checklevel==0 || numblocks == 0)
+    dataString.params = [checklevel,numblocks]
   else
     dataString.params = []
   return rpc.rpc_request(dataString, this.json)
 }
 
-Connect.prototype.verifychain = function(){
+Connect.prototype.verifytxoutproof = function(proof_string){
   /*
 
-  Arguments:
+  The verifytxoutproof method verifies that a proof
+  points to a transaction in a block. It returns the
+  transaction to which the proof is committed, or it
+  will throw an RPC error if the block is not in the
+  current best chain.
 
-  Response:
+  Arguments
+  "proof_string" 	(string, required) 	the hex-encoded proof
+                  generated by gettxoutproof
 
-  Examples:
-
-  */
-  let dataString = {"jsonrpc": "1.0", "id":"curltest"};
-  dataString.method = "";
-  if(dataString)
-    dataString.params = []
-  else
-    dataString.params = []
-  return rpc.rpc_request(dataString, this.json)
-}
-
-Connect.prototype.verifytxoutproof = function(){
-  /*
-
-  Arguments:
-
-  Response:
+  Response
+  "txid" 	(string) 	the transaction ids to which the proof commits; the array is empty if the proof is invalid
 
   Examples:
-
+  conn.verifytxoutproof(await conn.gettxoutproof(["51233ca562180a0c32e1977624320526b7d4026b40fc46832473e08452265fad"]))
   */
   let dataString = {"jsonrpc": "1.0", "id":"curltest"};
-  dataString.method = "";
-  if(dataString)
-    dataString.params = []
+  dataString.method = "verifytxoutproof";
+  if(proof_string)
+    dataString.params = [proof_string]
   else
     dataString.params = []
   return rpc.rpc_request(dataString, this.json)
